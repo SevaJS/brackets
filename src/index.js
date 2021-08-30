75c085b8a8d030a12c8f62eb874fabd8b0ee61ab
Semion <86849135+SevaJS@users.noreply.github.com>
2021-08-30 21:41:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = bracketsConfig.map(one => one[0]);
+  const closeBrackets = bracketsConfig.map(one => one[1]);
+  const stack = [];
+
+  for (let i of str) {
+    const closeIndex = closeBrackets.indexOf(i);
+    const openIndex = openBrackets.indexOf(i);
+
+    if (closeIndex === openIndex) {
+      if (stack[stack.length - 1] === openBrackets[openIndex]) {
+        stack.pop();
+      } else {
+        stack.push(openBrackets[openIndex]);
+      }
+      continue;
+    }
+
+    if (openIndex >= 0) {
+      stack.push(openIndex);
+      continue;
+    }
+
+    if (closeIndex !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return !stack.length;
+
+
 }
